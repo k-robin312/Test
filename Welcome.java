1c5bbb755e4c8b7569aea7f1598ea16be82c794c
k-robin312 <karlirichards94@gmail.com>
2026-02-11 13:23:06 -0500

    Update Welcome.java
    Add for loop

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,7 @@
 public class Welcome{
   public static void main(String[] args){
-    System.out.println("welcome");
+    for (int i = 1; i <5, i++){
+      System.out.println("welcome");
+    }
   }
 }
